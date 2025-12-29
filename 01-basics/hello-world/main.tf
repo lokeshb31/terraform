5d38b53987cb555b35c30e8023c43a1183d09368
lokeshb31 <lokeshborse@gmail.com>
2025-12-29 06:03:10 -0500

    Terraform first file with Git

--- 01-basics/hello-world/main.tf
+++ 01-basics/hello-world/main.tf
@@ -4,12 +4,7 @@ terraform {
 
 provider "local" {}
 
-resource "local_file" "file1" {
-  filename = "file1.txt"
-  content  = "First file"
-}
-
-resource "local_file" "file2" {
-  filename = "file2.txt"
-  content  = "Second file"
+resource "local_file" "hello" {
+  filename = var.filename
+  content  = var.content
 }
--- 01-basics/hello-world/variables.tf
+++ 01-basics/hello-world/variables.tf
@@ -1,10 +1,8 @@
 variable "filename" {
-  description = "Name of the file to create"
-  type        = string
-  default     = "hello.txt"
+  type    = string
+  default = "hello.txt"
 }
 
 variable "content" {
-  description = "File content"
-  type        = string
+  type = string
 }
